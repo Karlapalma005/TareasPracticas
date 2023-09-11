f16f90de5a15099f32a62cce84f8a80768d31afb
Karla Palma <143724796+Karlapalma005@users.noreply.github.com>
2023-09-11 17:52:58 -0600

    Add files via upload

---
+++ Practica (1).java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newclass;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica {
+    
+    public static void main(String[] args) {
+        
+            int base = 70;
+            int altura = 70;
+            
+            int AreaCuadrado = base*altura;
+            
+           var nombre = "Karla Guadalupe Palma Jiménez";
+           
+            System.out.println("El area del cuadrado en etero es " + AreaCuadrado + " y mi nombre es " + nombre);
+    }
+    
+}
