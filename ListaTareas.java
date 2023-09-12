53c6bc75b4c82d68667dd09f6d57d5f5a7ff0b4f
Karla Palma <143724796+Karlapalma005@users.noreply.github.com>
2023-09-11 18:07:08 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables1;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+    
+    public static void main(String[] args) {
+        double hora;
+        String a = "hago latarea";
+        String b = "me baño";
+        String c = "como";
+        String d = "me duermo";
+        String e = "me levanto";
+        final int llegada;
+        hora = (double 5.30);
+        llegada = 5;
+
+        System.out.println("En las mananas me deespierto a las " + hora + ", " + a + ", " + b + ", despues " + c + ", " + d + " a las " + llegada + " y " + e);
+    }
+    
+}
---
+++ tabla de las 5 cosas.psc
@@ -0,0 +1,9 @@
+Proceso tabladel5
+	Definir a1,b2 como entero;
+	Escribir "Tabla del 5";
+	Para a1<-1 Hasta 10 Con Paso 1 Hacer
+		b2<-5*a;
+		Escribir "5x",a,"=",b;
+	FinPara
+	
+FinProceso
